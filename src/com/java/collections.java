8bdbcc86bd13a9dec9d103b2387716e8e1e52537
Rooparamesh <rooparamesh9301@gmail.com>
2019-07-09 17:19:34 +0530

    updated files in next release

---
+++ src/com/java/collections.java
@@ -0,0 +1,10 @@
+package com.java;
+
+public class collections {
+
+	public static void main(String[] args) {
+	System.out.print("collectons");
+
+	}
+
+}
---
+++ src/com/selenium/gitrepos.java
@@ -0,0 +1,10 @@
+package com.selenium;
+
+public class gitrepos {
+
+	public static void main(String[] args) {
+		System.out.println("get repos");
+
+	}
+
+}
