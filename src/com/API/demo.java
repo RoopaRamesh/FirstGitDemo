9d366b5de3d01862863547d29bf83c4fe50cc921
Rooparamesh <rooparamesh9301@gmail.com>
2019-07-09 15:53:53 +0530

    first git repos selenium code

---
+++ src/com/API/demo.java
@@ -0,0 +1,12 @@
+package com.API;
+
+public class demo {
+
+	public static void main(String[] args) 
+	{
+	System.out.println("hello");
+	
+
+	}
+
+}
---
+++ src/com/selenium/secode.java
@@ -0,0 +1,9 @@
+package com.selenium;
+
+public class secode {
+
+	public static void main(String[] args) {
+	System.out.println("secode progms");
+	}
+
+}
