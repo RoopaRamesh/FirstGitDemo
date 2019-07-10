3ea4d1ac3000cf365bb55c4d5115b3d58c8d12e4
Rooparamesh <rooparamesh9301@gmail.com>
2019-07-10 12:15:25 +0530

    homepgae branch git commands added

--- src/com/selenium/gitrepos.java
+++ src/com/selenium/gitrepos.java
@@ -4,6 +4,13 @@ public class gitrepos {
 
 	public static void main(String[] args) {
 		System.out.println("get repos");
+		System.out.print("pull request");
+		System.out.println("git pull origin brnach");
+		System.out.println("git checkout branch");
+		System.out.println("git branch");
+		System.out.println("git status");
+		System.out.print("git add .");
+		System.out.println("git commit -m 'comment'");
 
 	}
 
